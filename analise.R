91539fadc900dc355363a3eb46fb05f3926b5b5b
Matheus Amaral <moesmatheus@gmail.com>
2019-09-28 21:32:28 +0000

    update plots

--- analise.R
+++ analise.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(esquisse)
 library(ggplot2)
+library(dplyr)
 
 #Ler dados de sales
 sales <- read_excel("Dados Trabalho.xlsx", 
@@ -11,52 +12,68 @@ inventory <- read_excel("Dados Trabalho.xlsx",
     sheet = "INVENTORY", skip = 2)
 
 #Esquisse com sales
-esquisse::esquisser(sales)
+#esquisse::esquisser(sales)
 
 #Esquisse com inventory
-esquisse::esquisser(inventory)
+#esquisse::esquisser(inventory)
+
+#sales = sales %>% mutate(`Discount%` = `Discount%` * 100)
 
 # Quantity x Discount
 ggplot(sales) +
   aes(x = Quantity, y = `Discount%`) +
   geom_point(size = 1L, colour = "#0c4c8a", position = "jitter") +
-  geom_smooth(method='lm',aes(x  = Quantity, y = `Discount%`))
-  
+  geom_smooth(method='lm',aes(x  = Quantity, y = `Discount%`)) +
+  labs(x = 'Quantidade vendida', y = 'Disconto (%)')
+ggsave('Quantity x Discount.pdf')  
 
-# Quantity x Discount
+# Price x Quantity
 ggplot(sales) +
   geom_jitter(aes(x = PRICE, y = Quantity),size = 0.7,colour = "#0c4c8a",width = 5) +
-  geom_smooth(method='lm',aes(x  = PRICE, y = Quantity))
-
+  geom_smooth(method='lm',aes(x  = PRICE, y = Quantity))+
+  labs(x = 'Preço', y = 'Quantidade vendida')
+ggsave('Price x Quantity.pdf') 
 
 # Price x Discount - Consequencia indireta das relacoes anteriores
 ggplot(sales) +
   aes(x = PRICE, y = `Discount%`) +
   geom_jitter(aes(x = PRICE, y = `Discount%`),size = 0.7,colour = "#0c4c8a",width = 5) +
-  geom_smooth(method='lm',aes(x  = PRICE, y = `Discount%`))
+  geom_smooth(method='lm',aes(x  = PRICE, y = `Discount%`))+
+  labs(x = 'Preço', y = 'Disconto (%)')
+ggsave('Price x Discount.pdf')
 
 # Venda por warehouse
 ggplot(sales) +
   aes(x = NAME...28, y = `Price after discount`) +
-  geom_col(fill = "#0c4c8a")
+  geom_col(fill = "#0c4c8a") +
+  labs(x = 'Warehouse', y = 'Total Sales')
+ggsave('Sales by warehouse.pdf')
 
 # Venda por sales representaive
 ggplot(sales) +
   aes(x = NAME...33, y = `Price after discount`) +
   geom_col(fill = "#0c4c8a") +
+  labs(x = 'Sales Representative', y = 'Total Sales') + 
   coord_flip()
+ggsave('Sales by representative.pdf')
 
 #Inventario por preco de prduto
-ggplot(inventory) +
-  geom_jitter(aes(x = Amount, y = PRICE), size = 1L, colour = "#0c4c8a", width = 2 )
+ggplot(inventory %>% mutate(PRICE = as.factor(inventory$PRICE))) +
+  aes(x = PRICE, y = Amount, fill = PRICE) +
+  labs(x = 'Preço', y = 'Quantidade em estoque') +
+  geom_boxplot()
+ggsave('Inventario por preco.pdf')
 
 #Inventario por data
 ggplot(inventory) +
   aes(x = DATE, y = Amount) +
-  geom_col(fill = "#0c4c8a")
-
+  geom_col(fill = "#0c4c8a") + 
+  labs(x = 'Data', y = 'Quantidade em estoque')
+ggsave('Inventario por data.pdf')
 
 #Inventario por warehouse
 ggplot(inventory) +
   aes(x = NAME...11, y = Amount) +
-  geom_boxplot(fill = "#0c4c8a")
+  geom_boxplot(fill = "#0c4c8a") +
+  labs(x = 'Warehouse', y = 'Quantidade em estoque')
+ggsave('Inventario por warehouse.pdf')
